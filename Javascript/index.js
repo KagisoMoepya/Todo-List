9a00bb9a75839a0b4ee71946b075c690be1f7ad8
KagisoMoepya <kagisomoepya19@gmail.com>
2022-10-06 13:47:51 +0200

    Recreating the javascript functionality

---
+++ Javascript/index.js
@@ -0,0 +1,11 @@
+export class List_Data {
+
+    constructor(list_name, remaining_tasks, tasks) {
+        this.list_name = list_name
+        this.remaining_tasks = remaining_tasks
+        this.tasks = tasks
+    }
+
+    
+
+}
